c80aa6a6d8ebdca2981d634f3a8bbf86f40e6fad
Renan Brando <reebrando@gmail.com>
2020-03-26 23:29:57 -0300

    Created session controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
+
+const ongController = require('../controllers/OngController')
+const incidentsController = require('../controllers/IncidentController')
+const profileController = require('../controllers/ProfileController')
+const sessionController = require('../controllers/SessionController')
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json('Hello')
-})
+router.post('/session', sessionController.create)
+
+router.get('/profile', profileController.list)
+
+router.get('/ongs', ongController.list)
+router.post('/ongs', ongController.create)
+
+router.get('/incidents', incidentsController.list)
+router.post('/incidents', incidentsController.create)
+router.delete('/incidents/:id', incidentsController.delete)
 
 module.exports = router
